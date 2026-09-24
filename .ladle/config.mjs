8711de0f90038b9464a18dc07b96b3ff99c6d863
Emiliya Yusupova <75200265+emiliyaYu@users.noreply.github.com>
2026-09-24 11:25:14 +0500

    feat: add ladle (#2)

---
+++ .ladle/config.mjs
@@ -0,0 +1,6 @@
+/** @type {import('@ladle/react').UserConfig} */
+export default {
+  // Автоматически открывать каталог компонентов в новой вкладке браузера.
+  // Возможные значения: 'tab' | 'window' | 'cross-tab' | 'none'.
+  open: 'tab',
+};
