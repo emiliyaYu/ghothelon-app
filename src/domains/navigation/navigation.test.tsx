a02da63133f8ff7a24c803602770a4be7c230565
Emiliya  Konyaeva <chikoch19@gmail.com>
2026-09-25 22:49:16 +0500

    feat: add header

--- src/domains/navigation/navigation.test.tsx
+++ src/domains/navigation/navigation.test.tsx
@@ -2,7 +2,7 @@
 import { cleanup, render, screen } from '@testing-library/react';
 import { MemoryRouter } from 'react-router-dom';
 import { afterEach, describe, expect, it } from 'vitest';
-import { Navigation } from './navigation';
+import { Navigation } from './navigation-component';
 
 afterEach(() => {
   cleanup();
